300b28c49352cfeecc12f3dfa40d7f1dbb495ca4
robertschaap <robert.schaap@mac.com>
2018-08-29 22:04:41 +0200

    added missing import to views.py

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from .models import Question
 
